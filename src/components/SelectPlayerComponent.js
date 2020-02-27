1002db21af81aef37a1f0c7730a08acb007cfb26
Senniinn <39193668+Senniinn@users.noreply.github.com>
2020-02-27 15:18:55 +0100

    player

--- src/components/SelectPlayerComponent.js
+++ src/components/SelectPlayerComponent.js
@@ -1,17 +1,53 @@
-import React from "react";
-import {Text, StyleSheet, View, Button, TextInput} from "react-native";
+import React, {useState} from "react";
+import {Text, StyleSheet, View, Button, FlatList} from "react-native";
 import { Input } from 'react-native-elements';
 
+
 class SelectPlayerComponent extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            players: [
+                {name: 'Jean', sipCount : 0, sipGiven : 0},
+                {name: 'Rene', sipCount : 0, sipGiven : 0},
+                {name: 'Bebe', sipCount : 0, sipGiven : 0},
+            ],
+            currentPlayer: ""
+        }
+    }
+
+    addPlayer(){
+        const newPlayer = {name: this.state.currentPlayer, sipCount : 0, sipGiven : 0};
+        this.setState({
+            players: [...this.state.players, newPlayer],
+            currentPlayer:""
+        })
+    }
+
     render() {
         return (
-            <View style={{ flex: 1, backgroundColor: 'yellow' }}>
-                <View style={{ flex: 0.66, backgroundColor: 'red' }}>
-                    <Input value="" placeholder="Enter email"/>
-                    <Input valye="" placeholder="Enter password"/>
+            <View style={ styles.container }>
+                <View style={ styles.header }>
+                    <Text  style={ styles.title }> Traquenard </Text>
                 </View>
-                <View style={{ flex: 0.33, backgroundColor: 'green' }}>
-                    <Button title="Login" backgroundColor="red" onPress={console.log(this)}/>
+                <View style={ styles.content }>
+                    <View style={ styles.list }>
+                        <FlatList
+                            data={this.state.players}
+                            renderItem={({ item }) => (
+                                <Text>{item.name}</Text>
+                            )}
+                        />
+                    </View>
+                    <Input
+                        placeholder='Ajouter un joueur'
+                        onChangeText={(text) => this.setState({currentPlayer:text})}
+                        value={this.state.currentPlayer}
+                    />
+                    <Button title="Solid Button" onPress={() => {
+                        this.addPlayer();
+                    }} />
                 </View>
             </View>
 
@@ -21,9 +57,25 @@ class SelectPlayerComponent extends React.Component {
 
 const styles = StyleSheet.create({
     container: {
-        width: "100%",
-        alignItems: 'center',
-        justifyContent: 'center',
+        flex: 1,
+        backgroundColor: '#fff',
+    },
+    content: {
+        padding: 40,
+    },
+    list: {
+        marginTop: 20,
+    },
+    header: {
+        height: 80,
+        padding: 38,
+        backgroundColor: 'red',
+    },
+    title: {
+        textAlign: 'center',
+        color: '#fff',
+        fontSize: 20,
+        fontWeight: 'bold',
     },
 });
 
