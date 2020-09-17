06d8db68a5cd79be3c3f98623aa3446fa5fdaa4a
alexis <alexis.heroin@epsi.fr>
2020-09-17 19:08:34 +0200

    change replace with replaceAll

--- src/components/helpers/FormattedText.js
+++ src/components/helpers/FormattedText.js
@@ -14,32 +14,32 @@ class FormattedTextComponent extends React.Component {
 
         if (text.includes("{currentPlayer}")) {
             const currentPlayer = gameReducer.players[gameReducer.currentPlayer];
-            text = text.replace("{currentPlayer}", currentPlayer.name)
+            text = text.replaceAll("{currentPlayer}", currentPlayer.name)
         }
         if (text.includes("{selectedPlayer}")) {
             const selectedPlayer = gameReducer.selectedPlayer;
-            text = text.replace("{selectedPlayer}", selectedPlayer.name)
+            text = text.replaceAll("{selectedPlayer}", selectedPlayer.name)
         }
         if (text.includes("{selectedCategory}")) {
             const selectedCategory = gameReducer.selectedCategory;
-            text = text.replace("{selectedCategory}", selectedCategory.name)
+            text = text.replaceAll("{selectedCategory}", selectedCategory.name)
         }
         if (text.includes("{sip}")) {
             const difficulty = gameReducer.difficulty;
             const sip = Math.round(parseInt(this.props.sip) * difficulty);
-            text = text.replace("{sip}", sip)
+            text = text.replaceAll("{sip}", sip)
         }
         if (text.includes("{playerX}")){
             const place = Math.floor(Math.random() * gameReducer.players.length);
             let player = gameReducer.players[place];
-            text = text.replace("{playerX}", player.name);
+            text = text.replaceAll("{playerX}", player.name);
             if (text.includes("{playerY}")) {
                 let place2 = Math.floor(Math.random() * gameReducer.players.length);
                 while (place2 === place){
                     place2 = Math.floor(Math.random() * gameReducer.players.length);
                 }
                 player = gameReducer.players[place2];
-                text = text.replace(/{playerY}/g, player.name)
+                text = text.replaceAll(/{playerY}/g, player.name)
             }
 
         }
