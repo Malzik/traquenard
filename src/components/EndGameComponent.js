cc5ade1186abb2dde3c610379f3c1fe37a039fdd
Senniinn <damien.genevee@epsi.fr>
2020-11-11 14:09:58 +0100

    feat : try first design end game

--- src/components/EndGameComponent.js
+++ src/components/EndGameComponent.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {StyleSheet, Text, View} from "react-native";
+import {FlatList, StyleSheet, Text, View} from "react-native";
 import {Button} from 'react-native-elements';
 import {bindActionCreators} from "redux";
 import * as gameActions from "../store/actions/gameAction";
@@ -40,16 +40,75 @@ class EndGameComponent extends React.Component {
     }
 
     render() {
+        const {players} = this.props.gameReducer;
+
         ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.PORTRAIT);
         return (
             <View style={ styles.container }>
                 <View style={ styles.header }>
                     <Text  style={ styles.title }> Fini </Text>
                 </View>
-                <View style={ styles.result }>
-                    <Table>
-                        <Rows data={this.state.tableData} style={styles.row} textStyle={styles.textTable}/>
-                    </Table>
+                <View style={ styles.middle }>
+                    <View>
+                        <View style={ styles.headRow }>
+                            <View style={ styles.headRowID }>
+                                <Text  style={ styles.textHead }> Place </Text>
+                            </View>
+                            <View style={ styles.headRowName }>
+                                <Text  style={ styles.textHead }> Name </Text>
+                            </View>
+                            <View style={ styles.headRowPoints }>
+                                <Text  style={ styles.textHead }> Points </Text>
+                            </View>
+                        </View>
+
+
+                        <View style={ styles.row }>
+                            <View style={ styles.headRowID }>
+                                <Text  style={ styles.textTable }> 1 </Text>
+                            </View>
+                            <View style={ styles.headRowName }>
+                                <Text  style={ styles.textTable }> Jean </Text>
+                            </View>
+                            <View style={ styles.headRowPoints }>
+                                <Text  style={ styles.textTable }> 15 </Text>
+                            </View>
+                        </View>
+                        <View style={ styles.row }>
+                            <View style={ styles.headRowID }>
+                                <Text  style={ styles.textTable }> 2 </Text>
+                            </View>
+                            <View style={ styles.headRowName }>
+                                <Text  style={ styles.textTable }> Pierre </Text>
+                            </View>
+                            <View style={ styles.headRowPoints }>
+                                <Text  style={ styles.textTable }> 12 </Text>
+                            </View>
+                        </View>
+                        <View style={ styles.row }>
+                            <View style={ styles.headRowID }>
+                                <Text  style={ styles.textTable }> 3 </Text>
+                            </View>
+                            <View style={ styles.headRowName }>
+                                <Text  style={ styles.textTable }> Paul </Text>
+                            </View>
+                            <View style={ styles.headRowPoints }>
+                                <Text  style={ styles.textTable }> 10 </Text>
+                            </View>
+                        </View>
+                        <View style={ styles.row }>
+                            <View style={ styles.headRowID }>
+                                <Text  style={ styles.textTable }> 4 </Text>
+                            </View>
+                            <View style={ styles.headRowName }>
+                                <Text  style={ styles.textTable }> Jack </Text>
+                            </View>
+                            <View style={ styles.headRowPoints }>
+                                <Text  style={ styles.textTable }> 8 </Text>
+                            </View>
+                        </View>
+
+                    </View>
                 </View>
                 <View style={ styles.bottom }>
                     <Button titleStyle={{textAlign: 'center', color: '#fff',
@@ -72,7 +131,7 @@ const styles = StyleSheet.create({
     header: {
         height: "20%",
     },
-    result: {
+    middle: {
         height: "50%",
         paddingHorizontal: "6%"
     },
@@ -87,8 +146,24 @@ const styles = StyleSheet.create({
         fontSize: wp('13%'),
         fontFamily: "titre"
     },
+    headRow:{
+        flexDirection: 'row',
+        borderBottomColor: 'white',
+        paddingBottom: wp('1%'),
+        borderBottomWidth: 3,
+    },
     row:{
-        marginTop: wp('10%'),
+        flexDirection: 'row',
+        marginTop: wp('3%'),
+        paddingBottom: wp('3%'),
+        borderBottomColor: 'white',
+        borderBottomWidth: 1,
+    },
+    textHead: {
+        fontSize: wp('7%'),
+        color: '#fff',
+        fontFamily: 'titre',
+        textAlign: 'center',
     },
     textTable: {
         fontSize: wp('6%'),
@@ -96,6 +171,15 @@ const styles = StyleSheet.create({
         fontFamily: 'gorgeesText',
         textAlign: 'center',
     },
+    headRowID: {
+        flex:0.3
+    },
+    headRowName: {
+        flex:0.4
+    },
+    headRowPoints: {
+        flex:0.3
+    }
 });
 
 const mapStateToProps = (state) => {
