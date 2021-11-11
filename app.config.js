0047d4e2b6729dae1098d34b4e247493c00d6d2e
Genevée <damien.genevee@epsi.fr>
2021-11-11 20:39:30 +0100

    update conf

--- app.config.js
+++ app.config.js
@@ -1,7 +1,42 @@
 export default () => {
     return {
-        "android": {
-            "package": "com.thetraquenard.corp"
+        "expo": {
+            "name": "Traquenard",
+            "slug": "Traquenard",
+            "privacy": "public",
+            "version": "1.0.1",
+            "orientation": "default",
+            "icon": "./assets/traquenardicon2.png",
+            "splash": {
+                "image": "./assets/traquenardicon.png",
+                "resizeMode": "contain",
+                "backgroundColor": "#2A2A2A"
+            },
+            "updates": {
+                "fallbackToCacheTimeout": 0
+            },
+            "assetBundlePatterns": [
+                "**/*"
+            ],
+            "ios": {
+                "supportsTablet": true,
+                "bundleIdentifier": "com.thetraquenard.corpo",
+                "buildNumber": "1.0.0"
+            },
+            "android": {
+                "package": "com.thetraquenard.corpo",
+                "playStoreUrl": "https://play.google.com/store/apps/details?id=com.thetraquenard.corpo",
+                "permissions": [],
+                "versionCode": 7,
+                "softwareKeyboardLayoutMode": "pan"
+            },
+            "web": {
+                "favicon": "./assets/traquenardicon.png"
+            },
+            "androidNavigationBar": {
+                "visible": "leanback",
+                "backgroundColor": "#2A2A2A"
+            }
         }
     };
 };
